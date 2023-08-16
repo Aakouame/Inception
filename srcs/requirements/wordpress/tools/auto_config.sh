a1a99990b298360f7600c0df01b9644717fbaf65
Aissa Kouame <akouame@e3r8p7.1337.ma>
2023-08-16 20:50:58 +0100

    finaly

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -30,4 +30,10 @@ if [ ! -d "/run/php" ]; then
     mkdir /run/php
 fi
 
+sed -i 's|listen = /run/php/php7.3-fpm.sock|listen = 9000|' /etc/php/7.3/fpm/pool.d/www.conf
+
 exec /usr/sbin/php-fpm7.3 -F
+
+
+# sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
+# sed -i 's|listen = /run/php/php7.3-fpm.sock|listen = 9000|' /etc/php/7.3/fpm/pool.d/www.conf
